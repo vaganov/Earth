40b8f3b0bad57bb6b6356178bebf820ae6537086
Nikita Vaganov <nikita.e.vaganov@gmail.com>
2017-10-28 18:48:38 +0300

    Load bigworld.csv into a GeoModel

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <QtCore>
 
+#include "GeoModel.h"
+#include "DefaultPainter.h"
+#include "ShapeLoader.h"
+
 int main (int argc, char* argv[]) {
     QCoreApplication app(argc, argv);
-    return app.exec();
+
+    GeoModel* model = new GeoModel();
+    AbstractPainter *painter = new DefaultPainter(model);
+
+    ShapeLoader shapeLoader("bigworld.csv", model, painter);
+    shapeLoader.load();
+
+    int res = app.exec();
+
+    delete painter;
+    delete model;
+
+    return res;
 }
